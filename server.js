7e32ea5182085ddd7bc99ae4f106fffa6f783137
Diana <info@campgroundguides.com>
2026-04-11 20:09:00 -0600

    Fix MongoDB connection block

--- server.js
+++ server.js
@@ -28,13 +28,9 @@ app.use(
 // MongoDB Connection
 // -------------------------------
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
-
 // -------------------------------
 // Database Schemas
 // -------------------------------
